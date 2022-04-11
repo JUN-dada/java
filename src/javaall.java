fc569bfe804973ce61c47d8b816b89a0f99a189f
JUN-dada <1129491453@qq.com>
2022-04-11 15:45:47 +0800

    Initial commit

--- src/javaall.java
+++ src/javaall.java
@@ -378,8 +378,6 @@ public class javaall {
         //随机同学
         String callname=randomStudent(students);
         System.out.println("被点到的同学是"+callname);
-        int a;
-        int b;
 
     }
 
